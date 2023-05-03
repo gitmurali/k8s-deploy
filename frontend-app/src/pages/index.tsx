cef1c9473bec101171022644949b69d1a9cf30bc
Murali ⚡️ <murali.nutalapati@sky.uk>
2023-05-03 15:27:07 +0100

    add frontend app

---
+++ frontend-app/src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="p-8 justify-center items-center h-screen flex">
+        <form className="flex">
+          <input
+            className="bg-gray-200 shadow-inner rounded-l p-2 flex-1"
+            id="email"
+            type="email"
+            aria-label="email address"
+            placeholder="Enter your email address"
+          />
+          <button
+            className="bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+}
