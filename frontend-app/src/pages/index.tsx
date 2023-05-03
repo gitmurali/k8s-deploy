49f00133342f52c7e1dcaa5262e1302d77403db5
Murali ⚡️ <murali.nutalapati@sky.uk>
2023-05-03 15:33:03 +0100

    add signup form

--- frontend-app/src/pages/index.tsx
+++ frontend-app/src/pages/index.tsx
@@ -8,14 +8,21 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="p-8 justify-center items-center h-screen flex">
-        <form className="flex">
+      <div className="p-8 justify-center items-center flex">
+        <form className="flex flex-col gap-y-2">
           <input
             className="bg-gray-200 shadow-inner rounded-l p-2 flex-1"
             id="email"
             type="email"
             aria-label="email address"
             placeholder="Enter your email address"
+          />
+          <input
+            className="bg-gray-200 shadow-inner rounded-l p-2 flex-1"
+            id="email"
+            type="password"
+            aria-label="password"
+            placeholder="password"
           />
           <button
             className="bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r"
